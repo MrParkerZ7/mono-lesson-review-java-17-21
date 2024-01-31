1e40a37c6f54b6a7a8a9b5025250850057e25ce5
MrParkerZ7 <neverzero9@gmail.com>
2024-01-31 12:15:16 +0700

    [update] unit test

--- 03-java-21-features/src/test/java/lesson/java21features/A_VirtualThreadTests.java
+++ 03-java-21-features/src/test/java/lesson/java21features/A_VirtualThreadTests.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class A_VirtualThreadTests {
 
     @Test
-    void testSampleMethod() throws InterruptedException {
+    void testSampleThread() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
 
         Thread virtualThread = Thread.ofVirtual().start(() -> {
@@ -24,5 +24,10 @@ class A_VirtualThreadTests {
         latch.await(5, TimeUnit.SECONDS);
         Thread.sleep(1000);
         assertFalse(virtualThread.isAlive());
+    }
+
+    @Test
+    void testSampleVirtualThread() throws InterruptedException {
+        assertDoesNotThrow(A_VirtualThread::sample);
     }
 }
