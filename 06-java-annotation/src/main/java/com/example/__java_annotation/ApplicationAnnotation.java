632981a6f3d750684a33479bb92682e1c2b578e9
Puck_CLI <puck.promsawat@gmail.com>
2024-07-19 11:59:16 +0700

    init lesson 06

---
+++ 06-java-annotation/src/main/java/com/example/__java_annotation/ApplicationAnnotation.java
@@ -0,0 +1,13 @@
+package com.example.__java_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationAnnotation {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationAnnotation.class, args);
+	}
+
+}
---
+++ 06-java-annotation/src/test/java/com/example/__java_annotation/ApplicationAnnotationTests.java
@@ -0,0 +1,13 @@
+package com.example.__java_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationAnnotationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
